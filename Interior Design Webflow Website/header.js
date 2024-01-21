6f2daace9ad0fee97fa4932b8b7549d77c6d6b71
stDem <61629062+stDem@users.noreply.github.com>
2024-01-22 00:30:27 +0300

    questions Homework_3

---
+++ Interior Design Webflow Website/header.js
@@ -0,0 +1,40 @@
+const Header = {
+  data() {
+    return {};
+  },
+  methods: {
+    sort() {},
+  },
+
+  template: ` <div class="container">
+    <header class="header">
+        <div class="header__logo-left">
+          <a class="interno-logo" href="#">
+            <svg
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+                fill="#CDA274"
+              />
+            </svg>
+            <h1 class="header__logo-left__text">Interno</h1>
+          </a>
+        </div>
+        <div class="header__logo-right">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+    </header>
+  </div>
+<section class="banner">
+
+</section>`,
+};
