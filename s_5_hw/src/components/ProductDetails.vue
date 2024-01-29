623636569577872826e88f2a5ad499b9c2e38137
stDem <61629062+stDem@users.noreply.github.com>
2024-01-29 22:46:21 +0300

    finished homework_5

---
+++ s_5_hw/src/components/ProductDetails.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="product in products" :key="product.id">
+    <p>Название: {{ product.name }}</p>
+    <p>Цена: {{ formattedPrice(product.price) }}</p>  
+    <p v-if="product.available === true">Available</p>
+    <p v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { id: 1, name: "Хлеб", price: 50, available: true },
+        { id: 2, name: "Молоко", price: 75, available: false },
+        { id: 3, name: "Соль", price: 20, available: true },
+        { id: 4, name: "Сахар", price: 63, available: false },
+      ],
+    };
+  },
+  computed: {
+      formattedPrice(price) {
+        let formattedNumber = price.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+          return formattedNumber;
+        //   return `$ ${price}`;
+        
+      
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
